d62b9335c6f65a6fa647d3bcad8e646380e943bb
Nelimee <adrien.suau@cerfacs.fr>
2021-09-17 14:59:30 +0200

    Updated build_ast.py to avoid being verbose each time

--- source/openqasm/tests/build_ast.py
+++ source/openqasm/tests/build_ast.py
@@ -6,22 +6,26 @@ from openqasm.parser.antlr.qasm_parser import parse
 HERE_DIR = Path("__file__").parent.absolute()
 EXAMPLE_DIR = HERE_DIR.parent.parent.parent / "examples"
 
-with open(EXAMPLE_DIR / "adder.qasm", "r") as f:
-    adder_qasm_str = f.read()
+with open(EXAMPLE_DIR / "ipe.qasm", "r") as f:
+    qasm_str = f.read()
 
-ast = parse(adder_qasm_str)
+ast = parse(qasm_str)
 
-print(f"Found {len(ast.statements)} statements.")
-print(f"Found OpenQASM version {ast.version['major']}.{ast.version['minor']}.")
 
-for i, statement in enumerate(ast.statements):
-    print(f"Statement {i} is of type {type(statement).__name__}.")
+verbose = False
 
-print("Listing for-loops...")
-for i, statement in enumerate(ast.statements):
-    if not isinstance(statement, ForInLoop):
-        continue
-    # We have the first for-loop
-    print(f"For-loop {i} body:")
-    for j, node in enumerate(statement.block):
-        print(f"  {j:>2} {type(node).__name__}")
+if verbose:
+    print(f"Found {len(ast.statements)} statements.")
+    print(f"Found OpenQASM version {ast.version['major']}.{ast.version['minor']}.")
+
+    for i, statement in enumerate(ast.statements):
+        print(f"Statement {i} is of type {type(statement).__name__}.")
+
+    print("Listing for-loops...")
+    for i, statement in enumerate(ast.statements):
+        if not isinstance(statement, ForInLoop):
+            continue
+        # We have the first for-loop
+        print(f"For-loop {i} body:")
+        for j, node in enumerate(statement.block):
+            print(f"  {j:>2} {type(node).__name__}")
