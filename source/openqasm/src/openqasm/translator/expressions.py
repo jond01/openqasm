e962486147cb8477aa8c9e2130ee216710649b4c
Nelimee <adrien.suau@cerfacs.fr>
2021-09-20 17:31:47 +0200

    Removed calls to eval in expression evaluation

--- source/openqasm/src/openqasm/translator/expressions.py
+++ source/openqasm/src/openqasm/translator/expressions.py
@@ -1,4 +1,5 @@
 import math
+import operator
 import typing as ty
 
 from openqasm.ast import (BinaryExpression, BooleanLiteral, Constant,
@@ -18,16 +19,40 @@ _CONSTANT_VALUES: ty.Dict[str, float] = {
 
 
 class _ComputeExpressionNamespace:
+
+    _BINARY_FUNCTIONS = {
+        ">": operator.__gt__,
+        "<": operator.__lt__,
+        ">=": operator.__ge__,
+        "<=": operator.__le__,
+        "==": operator.__eq__,
+        "!=": operator.__ne__,
+        "&&": None,
+        "||": None,
+        "|": operator.__or__,
+        "^": operator.__xor__,
+        "&": operator.__and__,
+        "<<": operator.__lshift__,
+        ">>": operator.__rshift__,
+        "+": operator.__add__,
+        "-": operator.__sub__,
+        "*": operator.__mul__,
+        "/": operator.__truediv__,
+        "%": operator.__mod__,
+        "**": operator.__pow__,
+    }
+    _UNARY_FUNCTIONS = {"-": operator.__neg__, "~": operator.__inv__, "!": operator.__not__}
+
     @staticmethod
     def compute_BinaryExpression(expr: BinaryExpression, context: OpenQASMContext) -> ty.Any:
         lhs = _ComputeExpressionNamespace.compute_Expression(expr.lhs, context)
         rhs = _ComputeExpressionNamespace.compute_Expression(expr.rhs, context)
-        return eval(f"{lhs} {expr.op.name} {rhs}")
+        return _ComputeExpressionNamespace._BINARY_FUNCTIONS[expr.op.name](lhs, rhs)
 
     @staticmethod
     def compute_UnaryExpression(expr: UnaryExpression, context: OpenQASMContext) -> ty.Any:
         value = _ComputeExpressionNamespace.compute_Expression(expr.expression, context)
-        return eval(f"{expr.op.name} {value}")
+        return _ComputeExpressionNamespace._UNARY_FUNCTIONS[expr.op.name](value)
 
     @staticmethod
     def compute_Constant(expr: Constant, context: OpenQASMContext) -> ty.Any:
