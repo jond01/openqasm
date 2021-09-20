a038bcb0e931701c80d06b9dc8e10e3f7866047b
Nelimee <adrien.suau@cerfacs.fr>
2021-09-20 17:38:11 +0200

    get_identifier now always return a list
    This helps in having a common interface no matter what is the type of
    the identifier and to be able to treat each identifier with only one
    piece of code.

--- source/openqasm/src/openqasm/translator/identifiers.py
+++ source/openqasm/src/openqasm/translator/identifiers.py
@@ -9,13 +9,13 @@ from openqasm.translator.expressions import compute_expression
 
 class _IdentifierRetrieverNamespace:
     @staticmethod
-    def get_Identifier(identifier: Identifier, context: OpenQASMContext) -> ty.Any:
-        return context.lookup(identifier.name, identifier.span)
+    def get_Identifier(identifier: Identifier, context: OpenQASMContext) -> ty.List:
+        return [context.lookup(identifier.name, identifier.span)]
 
     @staticmethod
-    def get_Subscript(identifier: Subscript, context: OpenQASMContext) -> ty.Any:
+    def get_Subscript(identifier: Subscript, context: OpenQASMContext) -> ty.List:
         index: int = compute_expression(identifier.index, context)
-        return context.lookup(identifier.name, identifier.span)[index]
+        return [context.lookup(identifier.name, identifier.span)[index]]
 
     @staticmethod
     def get_Selection(identifier: Selection, context: OpenQASMContext) -> ty.List:
@@ -51,7 +51,7 @@ class _IdentifierRetrieverNamespace:
 
 def get_identifier(
     identifier: ty.Union[Identifier, IndexIdentifier], context: OpenQASMContext
-) -> ty.Any:
+) -> ty.List:
     method_name: str = "get_" + type(identifier).__name__
     if not hasattr(_IdentifierRetrieverNamespace, method_name):
         raise UnsupportedFeature(
