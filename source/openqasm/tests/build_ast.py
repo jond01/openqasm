1bb07233c2d943260298cdd6344c497482943286
Nelimee <adrien.suau@cerfacs.fr>
2021-09-20 23:23:26 +0200

    Updated build_ast.py (WIP)

--- source/openqasm/tests/build_ast.py
+++ source/openqasm/tests/build_ast.py
@@ -6,11 +6,17 @@ from openqasm.parser.antlr.qasm_parser import parse
 HERE_DIR = Path("__file__").parent.absolute()
 EXAMPLE_DIR = HERE_DIR.parent.parent.parent / "examples"
 
-with open(EXAMPLE_DIR / "ipe.qasm", "r") as f:
+with open(EXAMPLE_DIR / "adder.qasm", "r") as f:
     qasm_str = f.read()
+with open(EXAMPLE_DIR / "stdgates.inc", "r") as f:
+    std_gates_inc = f.read()
+
+# test_stdgates = f"OPENQASM 3;\n{std_gates_inc}"
+# print(test_stdgates)
+# ast = parse(test_stdgates)
+qasm_str = qasm_str.replace('include "stdgates.inc";', std_gates_inc)
 
 ast = parse(qasm_str)
-
 
 verbose = False
 
@@ -31,9 +37,10 @@ if verbose:
             print(f"  {j:>2} {type(node).__name__}")
 
 
-try_translation = False
+try_translation = True
 if try_translation:
     from openqasm.translator.translator import OpenQASM3Translator
 
     translator = OpenQASM3Translator()
     circuit = translator.translate(ast)
+    print(circuit.draw())
